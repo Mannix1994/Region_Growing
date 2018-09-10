ca6d8dfeadfd46faf780b838bb92a99256e9983f
creator <wangmingzhang@outlook.com>
2018-09-10 16:16:05 +0800

    c

--- main.cpp
+++ main.cpp
@@ -2,20 +2,30 @@
 #include <opencv2/opencv.hpp>
 #include "region_grow/grow.h"
 #include "region_grow/timer.h"
+#include <future>
 
 using namespace std;
 using namespace cv;
 
 int main(int argc, char const **argv) {
+    cuda::setDevice(0);
+
     Timer timer;
     Mat src, denoise;
 
     src = imread(argv[1], CV_LOAD_IMAGE_COLOR);
     fastNlMeansDenoisingColored(src, denoise, 2);
     medianBlur(denoise, denoise, 3);
-    cuda::setDevice(0);
-    timer.rlog("part0");
-    cuda::GpuMat g_denoise = grow::BGR2Lab(denoise);
+    cuda::GpuMat g_denoise;
+
+    auto task = [&](){
+        g_denoise = grow::BGR2Lab(denoise);
+    };
+    thread thread1(task);
+    // 主线程可以左其他任务.main thread can do other work
+    // do other task...
+    // 等待转换颜色空间结束. waiting convert color finished
+    thread1.join();
     timer.rlog("part1");
 
     Mat edgeMap(denoise.rows, denoise.cols, CV_8UC3, Scalar(0, 0, 0));
@@ -27,13 +37,13 @@ int main(int argc, char const **argv) {
     M.start_grow(g_denoise, filled, edgeMap, 267, 108, grow::YELLOW, Size(200,200));
     timer.rlog("part3");
 
-    M.start_grow(g_denoise, filled, edgeMap, 91, 468, grow::GREEN);
+    M.start_grow(g_denoise, filled, edgeMap, 91, 468, grow::GREEN, Size(100,100));
     timer.rlog("part4");
 
-    M.start_grow(g_denoise, filled, edgeMap, 50, 173, grow::RED);
+    M.start_grow(g_denoise, filled, edgeMap, 50, 173, grow::RED, Size(100,100));
     timer.rlog("part5");
 
-    M.start_grow(g_denoise, filled, edgeMap, 247, 422, grow::WHITE);
+    M.start_grow(g_denoise, filled, edgeMap, 247, 422, grow::WHITE, Size(100,100));
     timer.rlog("part6");
 
 //    imshow("Original Image", denoise);
